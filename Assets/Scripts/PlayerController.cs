8477484b1cbf4ca477e8647e31e187a4e4519e4c
Arturo Nereu <hdgam3r@gmail.com>
2021-01-20 20:43:54 -0600

    set up scenes prefabs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float m_MovementSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+    }
+}
