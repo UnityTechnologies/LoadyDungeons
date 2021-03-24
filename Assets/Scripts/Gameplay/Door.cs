f1c30c6cb1f3344b572752a0c0bab077d9a616b9
Arturo Nereu <hdgam3r@gmail.com>
2021-03-23 20:51:26 -0500

    Modification to Gameplay prefabs

--- Assets/Scripts/Gameplay/Door.cs
+++ Assets/Scripts/Gameplay/Door.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField]
     private Animator m_Animator;
 
     // Start is called before the first frame update
